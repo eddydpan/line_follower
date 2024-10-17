23d72e7d447c53bff1f47c86d6f7e0a79a15ec26
eddydpan <eddydotpan@gmail.com>
2024-10-17 17:26:23 -0400

    Write documentation for send receive and line_follower

--- PythonReceiveData.py
+++ PythonReceiveData.py
@@ -6,13 +6,15 @@ arduinoComPort = "/dev/ttyACM0"
 baudRate = 9600
 serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
 
-serialOutputs = [] # In the form of a tuple with (leftIR, rightIR, leftMotorVel, rightMotorVel)
-num_timesteps = 0 # Initialize at 1
+num_timesteps = 0
 
+# Initialize outputs from Serial as separate lists
 leftIRs = []
 rightIRs = []
 leftMotorVels = []
 rightMotorVels = []
+
+
 try:
   while True:
 
@@ -28,9 +30,9 @@ try:
         rightMotorVels.append(int(data[3]))
         num_timesteps += 1
 
-except KeyboardInterrupt:
+except KeyboardInterrupt: # Use Ctrl+C as the way to finish the program 
   timesteps = range(num_timesteps)
-  data = {
+  data = { # Put each list into dictionary format
     'Timesteps': timesteps,
     'Left IRs' : leftIRs,
     'Right IRs' : rightIRs,
@@ -38,23 +40,25 @@ except KeyboardInterrupt:
     'Right Motor Speeds' : rightMotorVels
   }
 
-
+  # Create a dataframe out of the dictionary
   df = pd.DataFrame(data)
-  df.to_csv("LineFollowerData.csv")
+  df.to_csv("LineFollowerData.csv") # save it to a .csv file
+
+  # Initialize the figure
   fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
+  # Plot the analog voltages of the sensor data on the top subplot
   ax1.plot(timesteps, leftIRs)
   ax1.plot(timesteps, rightIRs)
   ax1.set_ylabel('Analog Voltage')
   ax1.legend()
 
-  # Plot the second dataset on the bottom subplot
+  # Plot the velocities on the bottom subplot
   ax2.plot(timesteps, leftMotorVels)
   ax2.plot(timesteps, rightMotorVels)
   ax2.set_xlabel('Timesteps')
   ax2.set_ylabel('DC Motor Velocity')
   ax2.legend()
-
 
   fig.suptitle('Motor Commands + IR Readings Over Time', fontsize=10)
   plt.show()
--- line_follower.ino
+++ line_follower.ino
@@ -6,99 +6,58 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor * leftMotor = AFMS.getMotor(3);
 Adafruit_DCMotor * rightMotor = AFMS.getMotor(4);
 
-int wideLeftIR = 0;
-int wideRightIR = 0;
+int leftIR;
+int rightIR;
 
-int wideLeftThresh = 690;//550 //745
-int wideRightThresh = 780;//765 //835
+int leftIRThresh = 795; // Initialize threshold for left IR
+int rightIRThresh = 805; // Initialize threshold for right IR
 
 int leftMotorVel;
 int rightMotorVel;
+
 void setup() {
-  // put your setup code here, to run once:
   long baudRate = 9600;
   AFMS.begin();
   Serial.begin(baudRate);
-  
-
+ 
 }
 
 void loop() {
+  // Run the motor backwards because of our mechanical orientation of the DC motors
   leftMotor->run(BACKWARD);
   rightMotor->run(BACKWARD);
+  // Get the IR sensors' analog voltage
+  rightIR = analogRead(3);
+  leftIR = analogRead(2);
 
-//  leftIR = analogRead(1);
-  wideRightIR = analogRead(3);
-//  rightIR = analogRead(2);
-  wideLeftIR = analogRead(2);
-  /*
-  Serial.print(wideLeftIR);
-  Serial.print(",");
-  Serial.print(wideRightIR);
-  Serial.println("");
-  */
-  leftMotor->setSpeed(leftMotorVel);
-  rightMotor->setSpeed(rightMotorVel);
-
-  
-
-  if(wideRightIR > wideRightThresh) {
+  if(rightIR > rightIRThresh) { // Right IR sensor sees the tape
+    // Turn right
     rightMotorVel = 0;
     leftMotorVel = 40;
   }
 
-  else if (wideLeftIR > wideLeftThresh) {
+  else if (leftIR > leftIRThresh) { // Left IR sensor sees the tape
+    // Turn left
     leftMotorVel = 0;
     rightMotorVel = 40;
   }
 
-  else {
+  else { // Neither of the sensors see the tape
+    // Go straight
     rightMotorVel = 20;
     leftMotorVel = 20;
   }
+
+  // Update motor speeds based on IR sensor values
   leftMotor->setSpeed(leftMotorVel);
   rightMotor->setSpeed(rightMotorVel);
-  
-//  Serial.print("Right_Motor_Vel:");
-//  Serial.print("Left_Sensor:");
-  Serial.print(wideLeftIR);
+
+  // Print out all of our outputs to receive on Python file in csv format
+  Serial.print(leftIR);
   Serial.print(",");
-//  Serial.print("Right_Sensor:");
-  Serial.print(wideRightIR);
-  
+  Serial.print(rightIR);
   Serial.print(",");
-//  Serial.print("Left_Motor_Vel:");
   Serial.print(leftMotorVel);
   Serial.print(",");
   Serial.println(rightMotorVel);
-  /*
-  if (leftIR < leftThresh) { // If true, left sensor no longer sees black tape = TURN RIGHT
-
-    rightMotor->setSpeed(22); // make the right motor slower to make a slight right
-    Serial.println("");
-    if (wideRightIR > wideRightThresh) { // if both left is off tape and wide right is on tape
-      leftMotor->setSpeed(35); // make left side faster for a sharper right turn 
-      if (rightIR < rightThresh) {
-        rightMotor->run(BACKWARD);
-        rightMotor->setSpeed(25); // make left side faster for a sharper right turn 
-        leftMotor->setSpeed(25); // make left side faster for a sharper right turn 
-
-      }
-    }
-    
-  }
-
-  else if (rightIR < rightThresh) { // If true, right sensor no longer sees black tape = TURN LEFT
-    leftMotor->setSpeed(22); // make the left motor slower to make a slight left
-    if (wideRightIR < wideRightThresh) { // if both right and wide right are off black tape
-      rightMotor->setSpeed(35); // make  right side faster for a sharper left turn
-      if (leftIR < leftThresh) {
-        leftMotor->run(BACKWARD);
-        leftMotor->setSpeed(25); // make left side faster for a sharper right turn 
-        rightMotor->setSpeed(25); // make left side faster for a sharper right turn 
-
-      }
-    }
-  }
-  */
 }
